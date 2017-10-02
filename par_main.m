aab0006e44689eea8b9ba2c00cc30db8f3797711
Simon D Angus <32443062+specialistgeneralist@users.noreply.github.com>
2017-10-02 23:31:47 +1100

    First commit

---
+++ par_main.m
@@ -0,0 +1,9 @@
+function out = par_main(inputs)
+
+% PAR_MAIN Example main program file for parallel execution.
+% .. code goes here ..
+% .. trivial example follows ..
+out{1} = inputs;
+out{2} = rand;
+pause(rand)
+
