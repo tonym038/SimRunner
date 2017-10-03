f69432150e0df30e818c0d17985e10c201eccddb
Simon D Angus <32443062+specialistgeneralist@users.noreply.github.com>
2017-10-03 12:24:46 +1100

    Minor file-name changes.
    As suggested by Ackermann, K.

--- SimRunner_par.m
+++ SimRunner_par.m
@@ -1,18 +1,18 @@
 function simrunner_par(RUNF, EXPNAME)
 
 % SIMRUNNER_PAR Run experiments in parallel.
-%    SIMRUNNER_PAR(RUNF, EXPNAME) requires that the main file 'par_main.m' is 
-%    on the path and, like 'test_main.m', accepts a structure of inputs 
-%    'inputs'. RUNF is the runfile containing the structured array CONSTANT 
-%    and vectors of experimental conditions contained in structured array 
-%    VARIABLE. EXPNAME is the output .mat file name to save.
+%    SIMRUNNER_PAR(RUNF, EXPNAME) requires that the (strictly named) main file 
+%    'main_par.m' is on the path and, like 'test_main.m', accepts a structure 
+%    of inputs 'inputs'.  RUNF is the runfile containing the structured array 
+%    CONSTANT and vectors of experimental conditions contained in structured 
+%    array VARIABLE.  EXPNAME is the output .mat file name to save.
 %
 %    Saves the same structure as in SIMRUNNER, however, due to the parallel 
 %    nature of this version, only saves the outcome once all parallel jobs are 
 %    finished.
 %
 %       Example: run the test runfile in parallel
-%          simrunner_par('test_runfile.txt', 'par_out')
+%          SimRunner_par('test_runfile.txt', 'par_out')
 %
 %    For more information on setup see the helpfile for SIMRUNNER.
 %
@@ -47,7 +47,7 @@ parfor ex = 1:n_ex
     % .. save inputs, evaluate model
     summary(ex).inputs  = inputs;
     summary(ex).start   = datestr(now);
-    summary(ex).results = par_main(inputs);
+    summary(ex).results = main_par(inputs);
     summary(ex).stop    = datestr(now);
 end
 % .. finish up, save
--- par_main.m
+++
@@ -1,9 +0,0 @@
-function out = par_main(inputs)
-
-% PAR_MAIN Example main program file for parallel execution.
-% .. code goes here ..
-% .. trivial example follows ..
-out{1} = inputs;
-out{2} = rand;
-pause(rand)
-
